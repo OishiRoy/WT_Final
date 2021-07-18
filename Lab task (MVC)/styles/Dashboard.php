876d1126e98193dc03834694d17f0aad2f9a9a2b
Anindita <oishiroy99@gmail.com>
2021-07-18 14:21:25 +0600

    Lab task

---
+++ Lab task (MVC)/styles/Dashboard.php
@@ -0,0 +1,52 @@
+<?php include 'admin_header.php';?>
+<!--dashboard starts -->
+<div>
+	<table  align="center">
+		<tr>
+			<td>
+				<div class="card">
+				<span class="text-white"> Total Students <br>
+					500
+				</span>
+				</div>
+			</td>
+			<td>
+				<div class="card">
+				<span class="text-white"> Total Students <br>
+					500
+				</span>
+				</div>
+			</td>
+			<td>
+				<div class="card">
+				<span class="text-white"> Total Students <br>
+					500
+				</span>
+				</div>
+			</td>
+		</tr>
+	</table>
+</div>
+<div class="center">
+	<h3 class="text">Student List</h3>
+	<table class="table table-striped">
+		<thead>
+			<th>Sl#</th>
+			<th> Name</th>
+			<th>DOB</th>
+			<th>Credit</th>
+			<th>CGPA</th>
+			<th>DEPT_ID</th>
+		</thead>
+		<tbody>
+			<td>1</td>
+			<td>Oishi</td>
+			<td>6</td>
+			<td>15</td>
+			<td>3.75</td>
+			<td>19-40001-1</td>
+		</tbody>
+	</table>
+</div>
+<!--dashboard ends -->
+<?php include 'admin_footer.php';?>
